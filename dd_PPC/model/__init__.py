7b65f57cc82e8c047cbf89ef7ee9c1c2186b1eda
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2025-12-11 21:41:57 +0900

    create the initial random_forest fit process

---
+++ dd_PPC/model/__init__.py
@@ -0,0 +1,4 @@
+from . import _model
+from ._model import *
+
+__all__ = _model.__all__.copy()
---
+++ dd_PPC/model/_model.py
@@ -0,0 +1,20 @@
+__all__ = []
+
+from matplotlib import pyplot as plt
+from sklearn.ensemble import RandomForestRegressor
+
+def fit_random_forest(x_train_std, y_train, show_fit_process: bool = True, show_pred_plot: bool = False):
+    _verbose = 2 if show_fit_process else 0
+
+    RF = RandomForestRegressor(verbose=_verbose)
+    RF.fit(x_train_std, y_train)
+
+    pred_RF = RF.predict(x_train_std)
+
+    if show_pred_plot:
+        plt.scatter(y_train, pred_RF - y_train)
+        plt.xlabel('y_train')
+        plt.ylabel('difference from y_train')
+        plt.show()
+
+    return RF, pred_RF
