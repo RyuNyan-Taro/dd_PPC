36b38ffe6b66a46fa0510b46feaf3f3596c995c5
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2025-12-13 18:55:02 +0900

    fix KeyError
    case: key name was not complete
    
    message: KeyError                                  Traceback (most recent call last)
    Cell In[8], line 1
    ----> 1 rf_model, pred_RF = dd_PPC.pipeline.apply_random_forest()
    
    File ~/Jupyter_python/dd_PPC/books/../codes/dd_PPC/pipeline/_pipeline.py:8, in apply_random_forest()
          6 _datas = file.get_datas()
          7 _datas_std = preprocess.standardized_with_numbers(_datas['train'])
    ----> 8 RF, pred_RF = model.fit_random_forest(_datas_std, _datas['target'])
         10 return RF, pred_RF
    
    KeyError: 'target'

--- dd_PPC/pipeline/_pipeline.py
+++ dd_PPC/pipeline/_pipeline.py
@@ -5,6 +5,6 @@ from .. import file, preprocess, model
 def apply_random_forest():
     _datas = file.get_datas()
     _datas_std = preprocess.standardized_with_numbers(_datas['train'])
-    RF, pred_RF = model.fit_random_forest(_datas_std, _datas['target'])
+    RF, pred_RF = model.fit_random_forest(_datas_std, _datas['target_consumption'])
 
     return RF, pred_RF
