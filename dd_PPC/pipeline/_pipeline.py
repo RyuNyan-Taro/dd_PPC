c1c44cc2a6e43e4798018c1830fc93f70122dfcb
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2025-12-13 18:51:15 +0900

    create ml pipeline to record the process

---
+++ dd_PPC/pipeline/_pipeline.py
@@ -0,0 +1,10 @@
+__all__ = ['apply_random_forest']
+
+from .. import file, preprocess, model
+
+def apply_random_forest():
+    _datas = file.get_datas()
+    _datas_std = preprocess.standardized_with_numbers(_datas['train'])
+    RF, pred_RF = model.fit_random_forest(_datas_std, _datas['target'])
+
+    return RF, pred_RF
