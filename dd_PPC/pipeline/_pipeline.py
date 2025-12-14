6c962a3ba50906fb38a6d5f2329feb08af3cdc4f
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2025-12-14 20:18:50 +0900

    modify to use category columns as number

--- dd_PPC/pipeline/_pipeline.py
+++ dd_PPC/pipeline/_pipeline.py
@@ -1,12 +1,18 @@
 __all__ = ['apply_random_forest']
 
+import pandas as pd
+
 from .. import file, preprocess, model
 
 def apply_random_forest():
     _datas = file.get_datas()
+
     _datas_std, _ = preprocess.standardized_with_numbers(_datas['train'])
+    _datas_category = preprocess.encoding_category(_datas['train'])
+
+    _x_train = pd.concat([_datas_std, _datas_category], axis=1)
     _y_train = _datas['target_consumption'].loc[:, 'cons_ppp17']
 
-    RF, pred_RF = model.fit_random_forest(_datas_std, _y_train)
+    RF, pred_RF = model.fit_random_forest(_x_train, _y_train)
 
     return RF, pred_RF
