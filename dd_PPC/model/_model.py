ac7d23ec3154531e9b1ba3866ecbe02e34ce721e
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2025-12-19 20:11:23 +0900

    revert n_estimators in RandomForestRegressor

--- dd_PPC/model/_model.py
+++ dd_PPC/model/_model.py
@@ -7,7 +7,7 @@ from sklearn.ensemble import RandomForestRegressor
 def fit_random_forest(x_train_std, y_train, show_fit_process: bool = True, show_pred_plot: bool = False) -> tuple[RandomForestRegressor, np.ndarray]:
     _verbose = 2 if show_fit_process else 0
 
-    RF = RandomForestRegressor(verbose=_verbose, n_estimators=1000)
+    RF = RandomForestRegressor(verbose=_verbose, n_estimators=100)
     RF.fit(x_train_std, y_train)
 
     pred_RF = RF.predict(x_train_std)
