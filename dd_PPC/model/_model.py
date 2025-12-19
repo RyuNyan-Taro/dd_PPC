c45f87dbb2fcc1ee71805423c045b97cb7a4439e
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2025-12-19 20:43:35 +0900

    add plot result

--- dd_PPC/model/_model.py
+++ dd_PPC/model/_model.py
@@ -22,10 +22,16 @@ def fit_random_forest(x_train_std, y_train, show_fit_process: bool = True, show_
     return RF, pred_RF
 
 
-def fit_lightgbm(x_train, y_train, seed: int = 42) -> tuple[lgb.LGBMRegressor, np.ndarray]:
+def fit_lightgbm(x_train, y_train, seed: int = 42, show_pred_plot: bool = False) -> tuple[lgb.LGBMRegressor, np.ndarray]:
     model = lgb.LGBMRegressor(random_state=seed)
     pred_y = model.fit(x_train, y_train)
 
     pred_lgb = pred_y.predict(x_train)
+
+    if show_pred_plot:
+        plt.scatter(y_train, pred_lgb - y_train)
+        plt.xlabel('y_train')
+        plt.ylabel('difference from y_train')
+        plt.show()
 
     return pred_y, pred_lgb
--- dd_PPC/pipeline/_lightgbm.py
+++ dd_PPC/pipeline/_lightgbm.py
@@ -6,15 +6,15 @@ from sklearn.preprocessing import StandardScaler
 
 from .. import file, preprocess, model
 
-def apply_lightgbm() -> tuple[lgb.LGBMRegressor, np.ndarray, StandardScaler]:
+def apply_lightgbm(show_pred_plot: bool = False) -> tuple[lgb.LGBMRegressor, np.ndarray, StandardScaler]:
     _datas = file.get_datas()
 
     _datas_std, sc = preprocess.standardized_with_numbers(_datas['train'])
     _datas_category = preprocess.encoding_category(_datas['train'])
 
     _x_train = np.hstack([_datas_std, _datas_category])
-    _y_train = _datas['target_consumption'].loc[:, 'cons_ppp17']
+    _y_train = _datas['target_consumption'].loc[:, 'cons_ppp17'].to_numpy()
 
-    LB, pred_LB = model.fit_lightgbm(_x_train, _y_train)
+    LB, pred_LB = model.fit_lightgbm(_x_train, _y_train, show_pred_plot=show_pred_plot)
 
     return LB, pred_LB, sc
