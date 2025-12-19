c0ca7f19ce28c88d0488f555ef41bae5c70193e6
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2025-12-19 21:07:01 +0900

    fix to use DataFrame

--- dd_PPC/pipeline/_lightgbm.py
+++ dd_PPC/pipeline/_lightgbm.py
@@ -2,6 +2,7 @@ __all__ = ['apply_lightgbm']
 
 import numpy as np
 import lightgbm as lgb
+import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
 from .. import file, preprocess, model
@@ -12,8 +13,8 @@ def apply_lightgbm(show_pred_plot: bool = False) -> tuple[lgb.LGBMRegressor, np.
     _datas_std, sc = preprocess.standardized_with_numbers_dataframe(_datas['train'])
     _datas_category = preprocess.encoding_category_dataframe(_datas['train'])
 
-    _x_train = np.hstack([_datas_std, _datas_category])
-    _y_train = _datas['target_consumption'].loc[:, 'cons_ppp17'].to_numpy()
+    _x_train = pd.concat([_datas_std, _datas_category], axis=1)
+    _y_train = _datas['target_consumption'].loc[:, 'cons_ppp17']
 
     LB, pred_LB = model.fit_lightgbm(_x_train, _y_train, show_pred_plot=show_pred_plot)
 
