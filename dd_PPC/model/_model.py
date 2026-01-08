0805cc14967c652f146996778700087b13d7694d
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2026-01-09 07:21:29 +0900

    fix to drop warnings for bagging parameters

--- dd_PPC/model/_model.py
+++ dd_PPC/model/_model.py
@@ -26,11 +26,9 @@ def fit_random_forest(x_train_std, y_train, show_fit_process: bool = True, show_
 
 
 def fit_lightgbm(x_train, y_train, seed: int = 42, categorical_cols: list[str] = None, show_pred_plot: bool = False) -> tuple[lgb.LGBMRegressor, np.ndarray]:
-    model = lgb.LGBMRegressor(random_state=seed, verbose=1, n_estimators=3000, force_row_wise=True, bagging_fraction=0.8, bagging_freq=5)
+    model = lgb.LGBMRegressor(random_state=seed, verbose=-1, n_estimators=3000, force_row_wise=True, bagging_fraction=0.8, bagging_freq=5)
 
-    with warnings.catch_warnings():
-        warnings.filterwarnings('ignore', message='.*bagging.*')
-        pred_y = model.fit(x_train, y_train, categorical_feature=categorical_cols if categorical_cols else 'auto')
+    pred_y = model.fit(x_train, y_train, categorical_feature=categorical_cols if categorical_cols else 'auto')
 
     pred_lgb = pred_y.predict(x_train)
 
