1ae871ec5ca24a318e7ded18e6f6f01d530d1491
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2026-01-09 18:52:17 +0900

    add xgboost_model_fit function for ansamble

--- dd_PPC/model/_model.py
+++ dd_PPC/model/_model.py
@@ -1,4 +1,10 @@
-__all__ = ['fit_random_forest', 'fit_lightgbm', 'fit_isotonic_regression', 'transform_isotonic_regression']
+__all__ = [
+    'fit_random_forest',
+    'fit_lightgbm',
+    'fit_xgboost',
+    'fit_isotonic_regression',
+    'transform_isotonic_regression'
+]
 
 import warnings
 import numpy as np
@@ -7,6 +13,7 @@ from matplotlib import pyplot as plt
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.isotonic import IsotonicRegression
 import lightgbm as lgb
+import xgboost as xgb
 
 def fit_random_forest(x_train_std, y_train, show_fit_process: bool = True, show_pred_plot: bool = False, seed: int = 42) -> tuple[RandomForestRegressor, np.ndarray]:
     _verbose = 2 if show_fit_process else 0
@@ -39,6 +46,21 @@ def fit_lightgbm(x_train, y_train, seed: int = 42, categorical_cols: list[str] =
         plt.show()
 
     return pred_y, pred_lgb
+
+
+def fit_xgboost(x_train, y_train, seed: int = 42, categorical_cols: list[str] = None, show_pred_plot: bool = False) -> tuple[xgb.XGBRegressor, np.ndarray]:
+    model = xgb.XGBRegressor(random_state=seed, verbose=-1, n_estimators=3000, force_row_wise=True, bagging_fraction=0.8, bagging_freq=5)
+
+    pred_y = model.fit(x_train, y_train, categorical_feature=categorical_cols if categorical_cols else 'auto')
+
+    pred_xgb = pred_y.predict(x_train)
+
+    if show_pred_plot:
+        plt.scatter(y_train, pred_xgb - y_train)
+        plt.xlabel('y_train')
+        plt.ylabel('difference from y_train')
+
+    return pred_y, pred_xgb
 
 
 def fit_isotonic_regression(pred_rate: pd.DataFrame, target_rate: pd.DataFrame) -> IsotonicRegression:
