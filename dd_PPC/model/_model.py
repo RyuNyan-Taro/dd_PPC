27ed4718870553a8dc9c11a7f8fc410574765f75
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2026-01-10 21:12:33 +0900

    drop debug print

--- dd_PPC/model/_model.py
+++ dd_PPC/model/_model.py
@@ -39,7 +39,6 @@ def fit_lightgbm(x_train, y_train, seed: int = 42, categorical_cols: list[str] =
             bagging_freq=5
         )
     params['random_state'] = seed
-    print(params)
 
     model = lgb.LGBMRegressor(**params)
 
