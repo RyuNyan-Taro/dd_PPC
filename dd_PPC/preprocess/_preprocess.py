0957aa6b1266ae434ce154b797c9afc20c41c2ae
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2026-01-11 20:30:20 +0900

    modify to use category columns from preprocess

--- dd_PPC/preprocess/_preprocess.py
+++ dd_PPC/preprocess/_preprocess.py
@@ -40,9 +40,13 @@ def standardized_with_numbers(train: pd.DataFrame, fit_model: StandardScaler | N
 
 def _standardized(train: pd.DataFrame, fit_model: StandardScaler | None = None) -> tuple[np.ndarray, list[str]]:
 
-    num_cols = ['weight', 'strata', 'hsize', 'age',
-                 'num_children5', 'num_children10', 'num_children18',
-                 'num_adult_female', 'num_adult_male', 'num_elderly', 'sworkershh', 'sfworkershh']
+    num_cols = [
+        'weight', 'strata', 'hsize', 'age',
+        'num_children5', 'num_children10', 'num_children18',
+        'num_adult_female', 'num_adult_male', 'num_elderly', 'sworkershh', 'sfworkershh',
+        # encoded category columns
+        'water_source', 'sanitation_source', 'dweltyp', 'educ_max', 'sector1d',
+    ]
 
     x_train = train[num_cols]
 
