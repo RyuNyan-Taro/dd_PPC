27197d1bad2194ad7faed3d13c0fe7a6c17857cd
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2026-01-11 18:30:02 +0900

    modify to delete debug log and latest parameters

--- dd_PPC/model/_model.py
+++ dd_PPC/model/_model.py
@@ -34,20 +34,10 @@ def fit_random_forest(x_train_std, y_train, show_fit_process: bool = True, seed:
 
 def fit_lightgbm(x_train, y_train, seed: int = 42, categorical_cols: list[str] = None, params: dict | None = None) -> tuple[lgb.LGBMRegressor, np.ndarray]:
     if params is None:
-        # params = dict(
-        #     verbose=-1,
-        #     n_estimators=3000,
-        #     force_row_wise=True,
-        #     bagging_fraction=0.8,
-        #     bagging_freq=5
-        # )
-        print('loaded params:', params)
-
         params = file.load_best_params('lightgbm')
         params['objective'] = 'regression'
         params['metric'] = 'rmse'
         params['verbose'] = -1
-        print('modified params:', params)
 
     params['random_state'] = seed
 
@@ -62,17 +52,10 @@ def fit_lightgbm(x_train, y_train, seed: int = 42, categorical_cols: list[str] =
 
 def fit_xgboost(x_train, y_train, seed: int = 42, params: dict | None = None) -> tuple[xgb.XGBRegressor, np.ndarray]:
     if params is None:
-        # params = dict(
-        #     n_estimators=200,
-        #     subsample=0.8
-        # )
         params = file.load_best_params('xgboost')
-        print('loaded params:', params)
 
         params['objective'] = 'reg:squarederror'
         params['eval_metric'] = 'rmse'
-        params['verbose'] = False
-        print('modified params:', params)
 
     params['random_state'] = seed
 
@@ -87,19 +70,10 @@ def fit_xgboost(x_train, y_train, seed: int = 42, params: dict | None = None) ->
 
 def fit_catboost(x_train, y_train, seed: int = 42, params: dict | None = None) -> tuple[CatBoost, Any]:
     if params is None:
-        # params = dict(
-        #     iterations=1100,
-        #     learning_rate=0.03,
-        #     depth=6,
-        #     loss_function='RMSE',
-        #     verbose=0
-        # )
         params = file.load_best_params('catboost')
 
-        print('loaded params:', params)
         params['verbose'] = 0
         params['loss_function'] = 'RMSE'
-        print('modified params:', params)
 
     params['random_state'] = seed
 
