1cfcf39044c03ac52814b0064954f2d100fadeaf
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2026-01-11 17:42:02 +0900

    revert to use multi seed models

--- dd_PPC/pipeline/_common.py
+++ dd_PPC/pipeline/_common.py
@@ -191,8 +191,8 @@ def _modeling_with_some_seeds(model_name: str, model_params: dict | None, x_trai
         random.seed(0)
         _seeds_length = 2
 
-        # seed_list = [123] + random.sample(range(1, 1000), _seeds_length)
-        seed_list = [123]
+        seed_list = [123] + random.sample(range(1, 1000), _seeds_length)
+        # seed_list = [123]
 
     model_with_preds = [
         getattr(model, f'fit_{model_name}')(x_train, y_train, seed=_seed, params=model_params)
