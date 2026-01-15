9bbe26c6b3108e26ef5724ce8732a747e71a48bc
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2026-01-15 20:46:35 +0900

    add infra svd variables

--- dd_PPC/pipeline/_common.py
+++ dd_PPC/pipeline/_common.py
@@ -29,7 +29,7 @@ def fit_and_test_model(
 
     def fit_data(train_x_, train_cons_y_, train_rate_y_):
 
-        x_train, sc, svd, _cat_cols = preprocess_data(train_x_)
+        x_train, sc, consumed_svd, infra_svd, _cat_cols = preprocess_data(train_x_)
         _y_train = _get_modified_target(train_cons_y_, boxcox_lambda)
 
         models, pred_vals = [], []
@@ -49,12 +49,12 @@ def fit_and_test_model(
 
         print('train comp score:', calc.weighted_average_of_consumption_and_poverty_rate(consumption, train_rate_y_, pred_rate_y))
 
-        return models, pred_vals, sc, svd, ir, consumption, pred_rate_y, x_train
+        return models, pred_vals, sc, consumed_svd, infra_svd, ir, consumption, pred_rate_y, x_train
 
 
-    def pred_data(test_x_, test_cons_y_, sc: StandardScaler, svd: TruncatedSVD, models: list, ir: IsotonicRegression):
+    def pred_data(test_x_, test_cons_y_, sc: StandardScaler, consumed_svd: TruncatedSVD, infra_svd: TruncatedSVD, models: list, ir: IsotonicRegression):
 
-        x_test, *_ = preprocess_data(test_x_, sc, svd=svd)
+        x_test, *_ = preprocess_data(test_x_, sc, consumed_svd=consumed_svd, infra_svd=infra_svd)
         pred_cons_ys = _fitting_with_some_models(models, x_test, boxcox_lambda)
 
         pred_cons_y = np.mean(pred_cons_ys, axis=0)
@@ -107,11 +107,11 @@ def fit_and_test_model(
             _datas['train'], _datas['target_consumption'], _datas['target_rate'], test_survey_ids=[_id]
         )
 
-        _models, _pred_vals, _sc, _svd, _ir, _consumption, _pred_rate_y, _x_train = fit_data(train_x, train_cons_y, train_rate_y)
+        _models, _pred_vals, _sc, _consumed_svd, _infra_svd, _ir, _consumption, _pred_rate_y, _x_train = fit_data(train_x, train_cons_y, train_rate_y)
 
         _train_metrics = calculate_metrics(_consumption['cons_pred'].to_numpy(), train_cons_y.loc[:, 'cons_ppp17'], _pred_rate_y, _consumption, _models, _x_train, train_rate_y)
 
-        _x_test, _y_test, _consumption, _pred_cons_y, _pred_rate_y = pred_data(test_x, test_cons_y, _sc, _svd, _models, _ir)
+        _x_test, _y_test, _consumption, _pred_cons_y, _pred_rate_y = pred_data(test_x, test_cons_y, _sc, _consumed_svd, _infra_svd, _models, _ir)
 
         _test_metrics = calculate_metrics(_pred_cons_y, _y_test, _pred_rate_y, _consumption, _models, _x_test, test_rate_y)
 
@@ -133,7 +133,7 @@ def fit_and_predictions_model(model_names: list[str], folder_prefix: str | None 
     _datas = file.get_datas()
 
     # learning
-    _x_train, _sc, _cat_cols = preprocess_data(_datas['train'])
+    _x_train, _sc, _consumed_svd, _infra_svd, _cat_cols = preprocess_data(_datas['train'])
     _y_train = _get_modified_target(_datas['target_consumption'])
 
     _models, pred_vals = [], []
@@ -171,16 +171,24 @@ def pred_models(fit_models: list, sc: StandardScaler) -> np.ndarray:
     return np.mean([_fit_model.predict(_datas) for _fit_model in fit_models], axis=0)
 
 
-def preprocess_data(datas: pd.DataFrame, sc: StandardScaler | None = None, consumed_svd: TruncatedSVD | None = None) -> tuple[pd.DataFrame, StandardScaler, TruncatedSVD, list[str]]:
+def preprocess_data(
+        datas: pd.DataFrame,
+        sc: StandardScaler | None = None,
+        consumed_svd: TruncatedSVD | None = None,
+        infra_svd: TruncatedSVD | None = None
+) -> tuple[pd.DataFrame, StandardScaler, TruncatedSVD, TruncatedSVD, list[str]]:
+    """Transforms input; returns normalized data and fitted objects"""
+
     _datas_category = preprocess.encoding_category_dataframe(datas)
     _datas_consumed, consumed_svd = preprocess.consumed_svd_dataframe(_datas_category, svd=consumed_svd)
+    _datas_infrastructure, infra_svd = preprocess.infrastructure_svd_dataframe(_datas_category, svd=infra_svd)
 
-    _new_datas = pd.concat([datas.copy().reset_index(drop=True), _datas_consumed.reset_index(drop=True)], axis=1)
-    _datas_std, sc = preprocess.standardized_with_numbers_dataframe(_new_datas, sc, add_columns=_datas_consumed.columns.tolist())
+    _new_datas = pd.concat([datas.copy().reset_index(drop=True), _datas_consumed.reset_index(drop=True), _datas_infrastructure.reset_index(drop=True)], axis=1)
+    _datas_std, sc = preprocess.standardized_with_numbers_dataframe(_new_datas, sc, add_columns=_datas_consumed.columns.tolist() + _datas_infrastructure.columns.tolist())
 
     category_cols = _datas_category.columns
 
-    return pd.concat([_datas_std.reset_index(drop=True), _datas_category.reset_index(drop=True)], axis=1), sc, consumed_svd, list(category_cols)
+    return pd.concat([_datas_std.reset_index(drop=True), _datas_category.reset_index(drop=True)], axis=1), sc, consumed_svd, infra_svd, list(category_cols)
 
 
 def _get_modified_target(targets: pd.DataFrame, boxcox_lambda: float | None = None) -> pd.DataFrame:
