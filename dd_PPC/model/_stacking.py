048be5d7af9956f548db615304735a566e4e1f46
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2026-01-20 20:29:16 +0900

    revert to drop worse importance columns for lightgbm

--- dd_PPC/model/_stacking.py
+++ dd_PPC/model/_stacking.py
@@ -246,11 +246,6 @@ def _get_initialized_model(model_name: str, model_params: dict, boxcox_lambda: f
 
             return [('count_encoding', _ce), ('drop_features', FunctionTransformer(_drop_catboost_features)), ('model', _model)]
 
-        if model_name == 'lightgbm':
-            def _drop_lightgbm_features(X):
-                return X.drop(columns=['sewer', 'sanitation_source', 'urban'])
-
-            return [('count_encoding', _ce), ('drop_features', FunctionTransformer(_drop_lightgbm_features)), ('model', _model)]
         return [('count_encoding', _ce), ('model', _model)]
 
         # return [('model', _model)]
