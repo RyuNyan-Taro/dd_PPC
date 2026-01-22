77ab06ddd36522c4bdf69e984b3353c8d0d00ff3
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2026-01-22 20:53:10 +0900

    fix KeyError to miss length

--- dd_PPC/preprocess/_preprocess.py
+++ dd_PPC/preprocess/_preprocess.py
@@ -152,22 +152,14 @@ def complex_numbers_dataframe(train: pd.DataFrame) -> pd.DataFrame:
     _sector_edu_mean = train.groupby('sector1d')['educ_max'].transform('mean')
 
     _concat_consumed_dict = {
-        '0000': 0,
-        '0001': 1,
-        '0010': 2,
-        '0011': 3,
-        '0100': 4,
-        '0101': 5,
-        '0110': 6,
-        '0111': 7,
-        '1000': 8,
-        '1001': 9,
-        '1010': 10,
-        '1011': 11,
-        '1100': 12,
-        '1101': 13,
-        '1110': 14,
-        '1111': 15
+        '000': 1,
+        '001': 2,
+        '010': 3,
+        '011': 4,
+        '100': 5,
+        '101': 6,
+        '110': 7,
+        '111': 8,
     }
 
     _complex_numbers = {
