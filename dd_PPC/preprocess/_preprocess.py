90a9e79e773fca5c10ff028dada4ca712bd371fe
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2026-01-25 18:30:07 +0900

    add ppp< 3 and any_nonagoric and sewer user not have consumed

--- dd_PPC/preprocess/_preprocess.py
+++ dd_PPC/preprocess/_preprocess.py
@@ -174,6 +174,9 @@ def complex_numbers_dataframe(train: pd.DataFrame) -> pd.DataFrame:
         'zscore_consumed_to_strata': (train['svd_consumed_0'] - (_strata_mean + 1e-6)) / (_strata_std + 1e-6),
         'concat_consumed': train[['consumed3100', 'consumed1500', 'consumed2000', 'consumed3000', 'consumed1800', 'consumed3100']].apply(
         lambda x: sum([_val for _val in x]), axis=1),
+        'lower_than_not_have_consumed': train[
+            ['consumed900', 'consumed4100', 'consumed300',]].apply(
+            lambda x: sum([_val for _val in x]), axis=1),
         'exp_per_hsize': train['utl_exp_ppp17'] / train['hsize'],
         'any_nonagoric_and_sewer': (train['any_nonagric'] + train['sewer']) / 2,
         # 'concat_consumed': train[
