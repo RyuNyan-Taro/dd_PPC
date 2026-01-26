6033381c132bb913b11bda1a3c085cd8dc215b21
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2026-01-26 21:09:56 +0900

    add and drop urban_sector_combo feature

--- dd_PPC/preprocess/_preprocess.py
+++ dd_PPC/preprocess/_preprocess.py
@@ -209,6 +209,7 @@ def complex_numbers_dataframe(train: pd.DataFrame) -> pd.DataFrame:
         'burden_factor': (train['hsize'] - train['sfworkershh']) / (train['sfworkershh'] + 1),
         'infra_cons_ratio': train['svd_infrastructure_0'] / (train['svd_consumed_0'] + 1e-6),
         # 'is_high_educ': (train['educ_max'] >= 5).astype(int)
+        # 'urban_sector_combo': train['urban'] * 10 + train['sector1d']
     }
 
     return pd.DataFrame(_complex_numbers)
